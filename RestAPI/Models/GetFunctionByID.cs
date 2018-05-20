8265fdfb78c3d59f8bdc942a97f57b5d0572ee28
Jitendra <jitendra.jadav@outlook.com>
2018-05-20 12:07:17 +0530

    Add project files.

---
+++ RestAPI/Models/GetFunctionByID.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestAPI.Models
+{
+    public class GetFunctionByID
+    {
+        [Key]
+        public Guid? FunctionByID { get; set; }
+        public string Values { get; set; }
+        // All the other properties.
+    }
+}
