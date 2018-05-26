e63316b69c36c048d80b07b56419626aae1f2f2d
Jitendra Jadav <jitendra.jadav@outlook.com>
2018-05-26 13:13:23 +0530

    More changes

---
+++ RestWPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
