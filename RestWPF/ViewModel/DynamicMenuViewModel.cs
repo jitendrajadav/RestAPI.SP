5286093f5bbc0779026a668e170b8951e1688567
Jitendra Jadav <jitendra.jadav@outlook.com>
2018-07-30 12:58:04 +0530

    dynamic menu added

---
+++ RestWPF/ViewModel/DynamicMenuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace RestWPF.ViewModel
+{
+    public class DynamicMenuViewModel 
+    {
+        public string Text { get; set; }
+
+        public IList<DynamicMenuViewModel> MenuItems { get; private set; }
+
+        public DynamicMenuViewModel()
+        {
+            MenuItems = new List<DynamicMenuViewModel>();
+
+        }
+    }
+}
---
+++ RestWPF/Views/DynamicMenuView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace RestWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for DynamicMenu.xaml
+    /// </summary>
+    public partial class DynamicMenuView : Window
+    {
+        public DynamicMenuView()
+        {
+            InitializeComponent();
+            DataContext = new MyThemesViewModel();
+        }
+
+        public class MyThemesViewModel
+        {
+            public ObservableCollection<MyData> Themes { get; set; } = new ObservableCollection<MyData>();
+
+            public MyThemesViewModel()
+            {
+                Themes.Add(new MyData("a"));
+                Themes.Add(new MyData("b"));
+                Themes.Add(new MyData("c"));
+                Themes.Add(new MyData("d"));
+            }
+        }
+    }
+
+    public class MyData
+    {
+        public ObservableCollection<MyData2> More { get; } = new ObservableCollection<MyData2>() { new MyData2("h") };
+
+        public MyData(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+    }
+    public class MyData2
+    {
+        public MyData2(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
+
